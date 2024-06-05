caff81894804636592befbd3656334335b63f021
anushka <anushkapragati999@gmail.com>
2024-06-05 13:02:40 +0530

    First changes

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarousel from '../components/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <>
+      <MainCarousel/>
+        <div>
+        other sections
+        </div>
+    </>
+   
+  )
+}
+
+export default HomePage;
